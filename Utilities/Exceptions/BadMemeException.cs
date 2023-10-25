e2be691fd13f566aca30a15ca76021ba0aa749cd
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-25 20:32:29 -0300

    Create BadMemeException.cs

---
+++ Utilities/Exceptions/BadMemeException.cs
@@ -0,0 +1,20 @@
+﻿// https://github.com/Willster419/RelhaxModpack/blob/master/RelhaxModpack/RelhaxModpack/Common/BadMemeException.cs
+
+namespace ForsakenShortcut.Utilities.Exceptions
+{
+    /// <summary>
+    /// An exception used mostly for mistakes that could happen during development, 'sanity check' type verification. And also for bad memes.
+    /// </summary>
+    [Serializable]
+    public class BadMemeException : Exception
+    {
+        /// <summary>
+        /// Throw a bad meme exception.
+        /// </summary>
+        /// <param name="message">The message to tell the developer why his meme is bad.</param>
+        public BadMemeException(string message) : base(message)
+        {
+
+        }
+    }
+}
